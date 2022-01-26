c21945f2bb360aef68e1b73e82b69515130859b7
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-01-26 20:46:42 +0600

    class component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import React from "react";
 import ReactDOM from "react-dom";
 
-function clock() {
-  return (
-    <h1 className="heading">
-      <span className="text">Time: {new Date().toLocaleTimeString()}</span>
-    </h1>
-  );
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">Time: {new Date().toLocaleTimeString()}</span>
+      </h1>
+    );
+  }
 }
-
-ReactDOM.render(clock(), document.getElementById("root"));
+ReactDOM.render(<Clock />, document.getElementById("root"));
