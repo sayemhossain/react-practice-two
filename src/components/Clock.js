af8839caa489fafad42389b3fd7c7610fb7b886a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-01-28 01:09:35 +0600

    new code

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.clockTimer = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
   render() {
     return (
       <h1 className="heading">
         <span className="text">
           Time:
-          {new Date().toLocaleTimeString(this.props.locale)}
+          {this.state.date.toLocaleTimeString(this.props.locale)}
         </span>
       </h1>
     );
